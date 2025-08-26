58b6088eca4c77fc08413b86ed4820adcd4e1226
701-01\student <sdt2495sc0003@edu.sca.ac.jp>
2025-08-26 12:29:45 +0900

    プレイヤージャンプ追加

---
+++ Assets/kokoro/script/PlayerCtr.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerClr : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float maxAngularSpeed = 360f;//��b�Ԃɉ���]��
+    public float angularAccel = 720f;//�؂�ւ��X�s�[�h  
+    public float jumpForce = 7f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    private float targetAngularVel = 0f;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    bool CheckGrounded()
+    {
+        Bounds bounds = GetComponent<Collider2D>().bounds;
+        float extraHeight = 0.1f;
+
+        RaycastHit2D hit = Physics2D.BoxCast(bounds.center, bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
+
+        return hit.collider != null;
+    }
+
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal"); // -1 or 0 or +1
+
+
+        Vector2 v = rb.linearVelocity;
+        v.x = inputX * moveSpeed;
+        rb.linearVelocity = v;
+
+
+        targetAngularVel = -inputX * maxAngularSpeed;
+
+
+        float currentAV = rb.angularVelocity;
+
+
+        float newAV = Mathf.MoveTowards(
+            currentAV,
+            targetAngularVel,
+            angularAccel * Time.deltaTime
+        );
+
+        rb.angularVelocity = newAV;
+
+        isGrounded = CheckGrounded();
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            if (groundCheck != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            }
+        }
+    }
+}
