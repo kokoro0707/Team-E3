8f930931ad1b1bbe1188410a2d2ced31cad76290
daisuke <sdt2495gp0006@edu.sca.ac.jp>
2025-09-06 14:49:41 +0900

    スキルツリー開いた際にゲームプレイが止まる関数追加

--- Assets/daisuke/Script/SkillTreeActive.cs
+++ Assets/daisuke/Script/SkillTreeActive.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UIElements;
 
 public class SkillTreeActive : MonoBehaviour
 {
     [SerializeField] private RectTransform SkillTreePanel;
     [SerializeField] private float slideDistance = 800f; // 下方向への移動量
     [SerializeField] private float slideSpeed = 5f;
+    [SerializeField] private float slideDelay = 0.3f; // 現状使ってない関数
 
     private bool isActive = false;
     private Vector2 visiblePosition;
@@ -27,7 +31,24 @@ public class SkillTreeActive : MonoBehaviour
     {
         isActive = !isActive;
 
+        if(isActive)
+        {
+            SkillTreePanel.gameObject.SetActive(true);
+            Time.timeScale = 0f; // ゲームの一時停止
+        }
+        else
+        {
+            Time.timeScale = 1f; // ゲーム再開
+        }
+
         targetPosition = isActive ? visiblePosition : hiddenPositin;
+
+        // スライドの処理にフェードやエフェクトを入れたい場合使用する、現状使わない
+        //if (!isActive)
+        //{
+        //    StopAllCoroutines();
+        //    StartCoroutine(DisableAfterSlide());
+        //}
     }
 
     private void Update()
@@ -35,9 +56,17 @@ public class SkillTreeActive : MonoBehaviour
         // スライドアニメーション
         SkillTreePanel.anchoredPosition = Vector2.Lerp(SkillTreePanel.anchoredPosition, targetPosition, Time.unscaledDeltaTime * slideSpeed);
 
+        // キーボード入力時にも表示
         if (Input.GetKeyDown(KeyCode.M))
         {
             ActiveSkillTree();
         }
+    }
+
+    private IEnumerator DisableAfterSlide()
+    {
+        yield return new WaitForSecondsRealtime(slideDelay);
+
+        SkillTreePanel.gameObject.SetActive(false);
     }
 }
