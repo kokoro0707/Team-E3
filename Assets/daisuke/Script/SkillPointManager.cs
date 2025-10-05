0b449f1609966036e17b260d0889ad7159c48a58
Daisuke <sdt2495gp0006@edu.sca.ac.jp>
2025-10-06 01:34:11 +0900

    スキルポイント消費処理追加

---
+++ Assets/daisuke/Script/SkillPointManager.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+public class SkillPointManager : MonoBehaviour
+{
+    public static SkillPointManager instance{ get; private set; }
+
+    [SerializeField] private int skillPoints = 0;
+    [SerializeField] private int killCount = 0;
+    [SerializeField] private int killsPoint = 2; // 2体で1ポイント
+
+    [SerializeField] private TextMeshProUGUI skillPointText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+            skillPoints = 3; // デバック用初期値
+    }
+
+    private void Start()
+    {
+        UpdateSkillPointUI();
+    }
+
+    private void UpdateSkillPointUI()
+    {
+        if(skillPointText != null)
+        {
+            skillPointText.text = $"Skill Point: {skillPoints}";
+        }
+    }
+
+    public int GetSkillPoint() => skillPoints;
+
+    // キルカウント増加
+    public void AddKillCount(int amont)
+    {
+        killCount += amont;
+
+        if(killCount >= killsPoint)
+        {
+            killCount = 0;
+            AddSkillPoint(1);
+        }
+    }
+
+    // スキルポイントを増やす
+    private void AddSkillPoint(int amont)
+    {
+        skillPoints += amont;
+        UpdateSkillPointUI();
+    }
+
+    // スキルポイント使用
+    public bool UseSkillPoint(int cost)
+    {
+        if(skillPoints >= cost)
+        {
+            skillPoints -= cost;
+            UpdateSkillPointUI();
+            return true;
+        }
+        else
+        {
+            Debug.Log("スキルポイントが足りません");
+            return false;
+        }
+
+    }
+}
